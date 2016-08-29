13319109198a01fbdb4d2e950412cc779372e948
Kevin A Namey <kan0010@tux054.eng.auburn.edu>
2016-08-29 14:28:08 -0500

    I am committing a homework assignment to my github account.

---
+++ hw1.cpp
@@ -0,0 +1,5 @@
+5	 a)  Jack Reeves views programming as more of a design activity as opposed to an engineering process.  
+	b)  Software is very cheap to build, so  there aren't rigid standards for writing code.  
+	c)  Languages like C++ are preferred over other languages because they are suitable for capturing high level design concepts.  They allow us to directly express high level information about design components, which makes it easier to produce the design and refine it later.
+	d)  Software engineering and design is different from other engineering processes in that it is very difficult to find any general purpose methods for validating software design because of the economics of the build/test cycle and the fact that a software system can represent practically anything.
+
